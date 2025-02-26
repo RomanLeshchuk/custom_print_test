1295dcc694928b691b6f42be3e96f3d698809506
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-02-27 01:36:46 +0200

    Added README.md

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+/*
+On my system, standard formatting was about four times faster,
+while two custom methods were about equally efficient.
+One of the reasons could be usage of high-level <iostream> library and
+not bothering too much about optimization in this custom implementation.
+*/
+
 #include "measure_performance.h"
 #include "example.h"
 
