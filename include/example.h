618239caa11697e7f891e231f945341193187fd5
RomanLeshchuk <romanleshchuk2000@gmail.com>
2025-02-27 20:41:26 +0200

    Added header guards

--- include/example.h
+++ include/example.h
@@ -1,5 +1,10 @@
 #pragma once
 
+#ifndef EXAPLE_H
+#define EXAPLE_H
+
 void myPrintExample();
 void myPrintfExample();
 void totalExample();
+
+#endif
--- include/measure_performance.h
+++ include/measure_performance.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#ifndef MEASURE_PERFORMANCE_H
+#define MEASURE_PERFORMANCE_H
+
 unsigned int testCustomTemplateFormatPrint(unsigned int inputSize);
 unsigned int testCustomLegacyFormatPrint(unsigned int inputSize);
 unsigned int testStandardFormatPrint(unsigned int inputSize);
 void comparePerformance();
+
+#endif
